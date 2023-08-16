687d2f00f127be11f85a97da4cafede9b09ed589
Andrea Scuderi <andreascuderi@ymail.com>
2023-08-16 18:28:58 +0200

    Add WebHook structure

---
+++ Sources/BreezeLambdaWebHook/APIGatewayV2Response+Extensions.swift
@@ -0,0 +1,55 @@
+//    Copyright 2023 (c) Andrea Scuderi - https://github.com/swift-sprinter
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+import struct AWSLambdaEvents.APIGatewayV2Response
+import struct AWSLambdaEvents.HTTPResponseStatus
+import class Foundation.JSONEncoder
+
+public extension APIGatewayV2Response {
+    private static let encoder = JSONEncoder()
+    
+    /// defaultHeaders
+    /// Override the headers in APIGatewayV2Response
+    static var defaultHeaders = [ "Content-Type": "application/json" ]
+
+    struct BodyError: Codable {
+        public let error: String
+    }
+    
+    /// init
+    /// - Parameters:
+    ///   - error: Error
+    ///   - statusCode: HTTP Status Code
+    init(with error: Error, statusCode: AWSLambdaEvents.HTTPResponseStatus) {
+        let bodyError = BodyError(error: String(describing: error))
+        self.init(with: bodyError, statusCode: statusCode)
+    }
+    
+    /// init
+    /// - Parameters:
+    ///   - object: Encodable Object
+    ///   - statusCode: HTTP Status Code
+    init<Output: Encodable>(with object: Output, statusCode: AWSLambdaEvents.HTTPResponseStatus) {
+        var body = "{}"
+        if let data = try? Self.encoder.encode(object) {
+            body = String(data: data, encoding: .utf8) ?? body
+        }
+        self.init(
+            statusCode: statusCode,
+            headers: APIGatewayV2Response.defaultHeaders,
+            body: body,
+            isBase64Encoded: false
+        )
+    }
+}
---
+++ Tests/BreezeLambdaWebHookTests/BreezeLambdaWebHookTests.swift
@@ -0,0 +1,82 @@
+//    Copyright 2023 (c) Andrea Scuderi - https://github.com/swift-sprinter
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+import AWSLambdaEvents
+import AWSLambdaRuntime
+import AWSLambdaTesting
+import AsyncHTTPClient
+@testable import BreezeLambdaWebHook
+import XCTest
+
+final class BreezeLambdaWebHookTests: XCTestCase {
+
+    let decoder = JSONDecoder()
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        setEnvironmentVar(name: "LOCAL_LAMBDA_SERVER_ENABLED", value: "true", overwrite: true)
+        LambdaInitializationContext.WebHook.timeout = 1
+    }
+
+    override func tearDownWithError() throws {
+        unsetenv("LOCAL_LAMBDA_SERVER_ENABLED")
+        unsetenv("_HANDLER")
+        LambdaInitializationContext.WebHook.timeout = 30
+        try super.tearDownWithError()
+    }
+    
+    func test_postWhenMissingBody_thenError() async throws {
+        setEnvironmentVar(name: "_HANDLER", value: "build/webhook.get", overwrite: true)
+        let createRequest = try Fixtures.fixture(name: Fixtures.getWebHook, type: "json")
+        let request = try decoder.decode(APIGatewayV2Request.self, from: createRequest)
+        let apiResponse: APIGatewayV2Response = try await Lambda.test(BreezeLambdaWebHook<String, MyPostWebHook>.self, with: request)
+        let response: APIGatewayV2Response.BodyError = try apiResponse.decodeBody()
+        XCTAssertEqual(apiResponse.statusCode, .badRequest)
+        XCTAssertEqual(apiResponse.headers, [ "Content-Type": "application/json" ])
+        XCTAssertEqual(response.error, "invalidRequest")
+    }
+    
+    func test_postWhenBody_thenValue() async throws {
+        setEnvironmentVar(name: "_HANDLER", value: "build/webhook.post", overwrite: true)
+        let createRequest = try Fixtures.fixture(name: Fixtures.postWebHook, type: "json")
+        let request = try decoder.decode(APIGatewayV2Request.self, from: createRequest)
+        let apiResponse: APIGatewayV2Response = try await Lambda.test(BreezeLambdaWebHook<String, MyPostWebHook>.self, with: request)
+        let response: String = try apiResponse.decodeBody()
+        XCTAssertEqual(apiResponse.statusCode, .ok)
+        XCTAssertEqual(apiResponse.headers, [ "Content-Type": "application/json" ])
+        XCTAssertEqual(response, "body value")
+    }
+    
+    func test_getWhenMissingQuery_thenError() async throws {
+        setEnvironmentVar(name: "_HANDLER", value: "build/webhook.get", overwrite: true)
+        let createRequest = try Fixtures.fixture(name: Fixtures.postWebHook, type: "json")
+        let request = try decoder.decode(APIGatewayV2Request.self, from: createRequest)
+        let apiResponse: APIGatewayV2Response = try await Lambda.test(BreezeLambdaWebHook<String, MyGetWebHook>.self, with: request)
+        let response: APIGatewayV2Response.BodyError = try apiResponse.decodeBody()
+        XCTAssertEqual(apiResponse.statusCode, .badRequest)
+        XCTAssertEqual(apiResponse.headers, [ "Content-Type": "application/json" ])
+        XCTAssertEqual(response.error, "invalidRequest")
+    }
+    
+    func test_getWhenQuery_thenValue() async throws {
+        setEnvironmentVar(name: "_HANDLER", value: "build/webhook.post", overwrite: true)
+        let createRequest = try Fixtures.fixture(name: Fixtures.getWebHook, type: "json")
+        let request = try decoder.decode(APIGatewayV2Request.self, from: createRequest)
+        let apiResponse: APIGatewayV2Response = try await Lambda.test(BreezeLambdaWebHook<String, MyGetWebHook>.self, with: request)
+        let response: [String: String] = try apiResponse.decodeBody()
+        XCTAssertEqual(apiResponse.statusCode, .ok)
+        XCTAssertEqual(apiResponse.headers, [ "Content-Type": "application/json" ])
+        XCTAssertEqual(response.count, 2)
+    }
+}
