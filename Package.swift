1eee5e00a1d3a60aad2a2f63330d29e7a0682747
Andrea Scuderi <andreascuderi@ymail.com>
2023-12-27 09:44:26 +0100

    Update swift-sls-adapter to 0.2.1

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
         .package(url: "https://github.com/stencilproject/Stencil.git", from: "0.15.1"),
-        .package(url: "https://github.com/swift-sprinter/swift-sls-adapter", from: "0.2.0"),
+        .package(url: "https://github.com/swift-sprinter/swift-sls-adapter", from: "0.2.1"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.11.2"),
     ],
     targets: [
