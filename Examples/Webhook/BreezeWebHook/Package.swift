20bc978bf11cd1af77856b58ae65b6f00ec94e4c
Andrea Scuderi <andrea.scuderi@ymail.com>
2025-07-30 20:31:04 +0100

    Fix BreezeWebHook

--- Examples/Webhook/BreezeWebHook/Package.swift
+++ Examples/Webhook/BreezeWebHook/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swift-serverless/BreezeLambdaWebHook.git", from: "1.0.0-beta.1"),
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "2.0.0-beta.1"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "2.0.0-beta.1")
     ],
     targets: [
         .executableTarget(
